e187e8f3b52d11da5fff137913448063414c711b
tancaibao <tancaibao@gmail.com>
2012-11-11 23:54:50 +0800

    Update src/scrame/RawCase.java
    Added student course testcase

--- src/scrame/RawCase.java
+++ src/scrame/RawCase.java
@@ -83,4 +83,133 @@ public class RawCase  {
 
 		SerializeDB.writeSerializedObject("course.dat", list);
 	}
+		public void rawStudentCourse()
+	{
+		List<StudentCourse> list= StudentCourse.getRegisterList();
+		StudentCourse[] sc = new StudentCourse[500];
+		 sc[0] = new StudentCourse("U1234567A", "CZ0001");
+		 sc[1] = new StudentCourse("U1234564Z", "CZ0001");
+		 sc[2] = new StudentCourse("U1234567T", "CZ0001");	 		 
+		 sc[3] = new StudentCourse("U1234517L", "CZ0001");
+		 sc[4] = new StudentCourse("U1234562Z", "CZ0001");
+		 sc[5] = new StudentCourse("U1234567R", "CZ0001");
+		 sc[6] = new StudentCourse("U1234527X", "CZ0001");
+		 sc[7] = new StudentCourse("U1234563Z", "CZ0001");
+		 sc[8] = new StudentCourse("U1234527D", "CZ0001");
+		 sc[9] = new StudentCourse("U1234567P", "CZ0001");
+		 sc[10] = new StudentCourse("U1234563C", "CZ0001");
+		 sc[11] = new StudentCourse("U1234517L", "CZ0001");
+		 sc[12] = new StudentCourse("U1234267Z", "CZ0001");
+		 sc[13] = new StudentCourse("U1233567C", "CZ0001");
+		 sc[14] = new StudentCourse("U1234567O", "CZ0001");
+		 sc[15] = new StudentCourse("U1233567M", "CZ0001");
+		 sc[16] = new StudentCourse("U1234561Z", "CZ0001");
+		 sc[17] = new StudentCourse("U1233367A", "CZ0001");
+		 
+		 sc[18] = new StudentCourse("U1234567A", "CZ1008");
+		 sc[19] = new StudentCourse("U1234267Z", "CZ1008");	
+		 sc[20] = new StudentCourse("U1234567T", "CZ1008");	
+		 sc[21] = new StudentCourse("U1234517L", "CZ1008");
+		 sc[22] = new StudentCourse("U1234562Z", "CZ1008");
+		 sc[23] = new StudentCourse("U1234567R", "CZ1008");
+		 sc[24] = new StudentCourse("U1234527X", "CZ1008");
+		 sc[25] = new StudentCourse("U1234563Z", "CZ1008");
+		 sc[26] = new StudentCourse("U1234527D", "CZ1008");
+		 sc[27] = new StudentCourse("U1234567P", "CZ1008");
+		 sc[28] = new StudentCourse("U1234563C", "CZ1008");
+		 sc[29] = new StudentCourse("U1234517L", "CZ1008");
+		 sc[30] = new StudentCourse("U1234567Y", "CZ1008");
+		 sc[31] = new StudentCourse("U1233567C", "CZ1008");
+		 sc[32] = new StudentCourse("U1234567O", "CZ1008");
+		 sc[33] = new StudentCourse("U1233567M", "CZ1008");
+		 sc[34] = new StudentCourse("U1234561Z", "CZ1008");
+		 sc[35] = new StudentCourse("U1233367A", "CZ1008");
+		 
+		 sc[36] = new StudentCourse("U1234567A", "CZ1002");
+		 sc[37] = new StudentCourse("U1234564Z", "CZ1002");
+		 sc[38] = new StudentCourse("U1234567T", "CZ1002");	 		 
+		 sc[39] = new StudentCourse("U1234517L", "CZ1002");
+		 sc[40] = new StudentCourse("U1234562Z", "CZ1002");
+		 sc[41] = new StudentCourse("U1234567R", "CZ1002");
+		 sc[42] = new StudentCourse("U1234527X", "CZ1002");
+		 sc[43] = new StudentCourse("U1234563Z", "CZ1002");
+		 sc[44] = new StudentCourse("U1234527D", "CZ1002");
+		 sc[45] = new StudentCourse("U1234567P", "CZ1002");
+		 sc[46] = new StudentCourse("U1234563C", "CZ1002");
+		 sc[47] = new StudentCourse("U1234517L", "CZ1002");
+		 sc[48] = new StudentCourse("U1234267Z", "CZ1002");
+		 sc[49] = new StudentCourse("U1233567C", "CZ1002");
+		 sc[50] = new StudentCourse("U1234567O", "CZ1002");
+		 sc[51] = new StudentCourse("U1233567M", "CZ1002");
+		 sc[52] = new StudentCourse("U1234561Z", "CZ1002");
+		 sc[53] = new StudentCourse("U1233367A", "CZ1002");
+		 
+		 sc[54] = new StudentCourse("U1234567A", "CZ2002");
+		 sc[55] = new StudentCourse("U1121137B", "CZ2002");
+		 sc[56] = new StudentCourse("U1233567C", "CZ2002");
+		 sc[57] = new StudentCourse("U1234577D", "CZ2002");		 
+		 sc[58] = new StudentCourse("U1235567A", "CZ2002");	
+		 sc[59] = new StudentCourse("U1234267Z", "CZ2002");			 
+		 sc[60] = new StudentCourse("U1132567D", "CZ2002");
+		 sc[61] = new StudentCourse("U1234597G", "CZ2002");
+		 sc[62] = new StudentCourse("U1233567F", "CZ2002");		 
+		 sc[63] = new StudentCourse("U1234567P", "CZ2002");
+		 sc[64] = new StudentCourse("U1234367C", "CZ2002");
+		 sc[65] = new StudentCourse("U1234567R", "CZ2002");
+		 sc[66] = new StudentCourse("U1234527X", "CZ2002");	
+		 sc[67] = new StudentCourse("U1234567Y", "CZ2002");	
+		 sc[68] = new StudentCourse("U1234567Z", "CZ2002");
+	 
+		 sc[69] = new StudentCourse("U1234567A", "CZ8003");
+		 sc[70] = new StudentCourse("U1121137B", "CZ8003");
+		 sc[71] = new StudentCourse("U1233567C", "CZ8003");
+		 sc[72] = new StudentCourse("U1234577D", "CZ8003");		 
+		 sc[73] = new StudentCourse("U1235567A", "CZ8003");	
+		 sc[74] = new StudentCourse("U1234267Z", "CZ8003");			 
+		 sc[75] = new StudentCourse("U1132567D", "CZ8003");
+		 sc[76] = new StudentCourse("U1234597G", "CZ8003");
+		 sc[77] = new StudentCourse("U1233567F", "CZ8003");		 
+		 sc[78] = new StudentCourse("U1234567P", "CZ8003");
+		 sc[79] = new StudentCourse("U1234367C", "CZ8003");
+		 sc[80] = new StudentCourse("U1234567R", "CZ8003");
+		 sc[81] = new StudentCourse("U1234527X", "CZ8003");	
+		 sc[82] = new StudentCourse("U1234567Y", "CZ8003");	
+		 sc[83] = new StudentCourse("U1234567Z", "CZ8003");
+
+		 sc[84] = new StudentCourse("U1234567A", "CZ2006");
+		 sc[85] = new StudentCourse("U1121137B", "CZ2006");
+		 sc[86] = new StudentCourse("U1233567C", "CZ2006");
+		 sc[87] = new StudentCourse("U1234577D", "CZ2006");		 
+		 sc[88] = new StudentCourse("U1235567A", "CZ2006");	
+		 sc[89] = new StudentCourse("U1234267Z", "CZ2006");			 
+		 sc[90] = new StudentCourse("U1132567D", "CZ2006");
+		 sc[91] = new StudentCourse("U1234597G", "CZ2006");
+		 sc[92] = new StudentCourse("U1233567F", "CZ2006");		 
+		 sc[93] = new StudentCourse("U1234567P", "CZ2006");
+		 sc[94] = new StudentCourse("U1234367C", "CZ2006");
+		 sc[95] = new StudentCourse("U1234567R", "CZ2006");
+		 sc[96] = new StudentCourse("U1234527X", "CZ2006");	
+		 sc[97] = new StudentCourse("U1234567Y", "CZ2006");	
+		 sc[98] = new StudentCourse("U1234567Z", "CZ2006");
+		 for (int i =0; i <99;i++)
+			list.add(sc[i]);
+
+		SerializeDB.writeSerializedObject("studentcourse.dat", list);
+	
+	}
+	
+	//	public void rawClass() {
+//		List<Class> list= Class.getClassList();
+//		Class[] cl = new Class[40];
+//		 cl[0] = new Class("FE1", "CZ2002", 20);	
+//		 cl[1] = new Class("FE2", "CZ2006", 30);	
+//		 cl[2] = new Class("FE3", "CZ8003", 15);		
+//		 cl[3] = new Class("FE4", "CZ1002", 30);	
+//		 cl[4] = new Class("FE5", "CZ1008", 16);	
+//		 cl[5] = new Class("FE6", "CZ0001", 17);	
+//		 for (int i =0; i <6;i++)
+//			list.add(cl[i]);
+//
+//		SerializeDB.writeSerializedObject("class.dat", list);
+//	}
 }
