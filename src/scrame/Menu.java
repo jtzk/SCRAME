111bf1416c42ae6de61fc6118873706164bca391
tancaibao <tancaibao@gmail.com>
2012-11-13 01:48:40 +0800

    Update src/scrame/Menu.java
    added coursecomponentmenu

--- src/scrame/Menu.java
+++ src/scrame/Menu.java
@@ -1,7 +1,9 @@
 package scrame;
 
-
 public class Menu {
+	private int max=100;
+	private int balance=0;
+	private int remain=0;
 	public static void showMenu() {
 		System.out.println();
 		System.out.println("Please choose an option from the menu below:");
@@ -44,4 +46,90 @@ public class Menu {
 		System.out.println("Terminating application...");
 		System.exit(0);
 	}
+	
+
+	public  void CourseComponentMenu(String _courseCode)
+		{
+			int componentAmount=0,choice=0,sumUp=0;
+			System.out.print("Enter component amount: ");
+			componentAmount=GetType.getInt();
+			while(componentAmount>0)
+			{
+				System.out.println("Select the component");
+				System.out.println("1)Assignment");
+				System.out.println("2)Quiz");
+				System.out.println("3)Report");
+				System.out.println("4)Exam");	
+				System.out.println("5)Tutorial");
+				System.out.printf("\nEnter your choice => ");
+				choice= GetType.getInt();
+				switch (choice)
+				{
+					case 1:
+//						System.out.print("coursecode "+_courseCode);
+//						sumUp=Assignment.sumUp(_courseCode);
+//						System.out.println("total"+sumUp);
+//						if(sumUp<100)
+//						{
+//							remain=Assignment.add(_courseCode, max);
+//							System.out.print("remain "+remain);
+//							if (remain==-1){
+//								//increase counter to request user add 1 more component
+//								componentAmount+=1;
+//								System.out.println("Component percent exceeded.");
+//							}
+//							else if(remain >0){
+//								//increase counter to request user add 1 more component
+//								componentAmount+=1;
+//								System.out.println("Component percent not full.");
+//							}
+					//	}
+						
+						
+						break;
+						
+					case 2:
+						
+						break;
+						
+					case 3:
+						
+						break;
+						
+					case 4: 
+							
+						break;
+						
+					case 5:
+						System.out.print("coursecode "+_courseCode);
+						
+						sumUp=TutorialEx.sumUp(_courseCode);
+						//System.out.println("total"+sumUp);
+						if(sumUp<100)
+						{
+							remain=TutorialEx.addTutorial(_courseCode, max);
+							//System.out.print("remain "+remain);
+							if (remain==-1){
+								//increase counter to request user add 1 more component
+								componentAmount+=1;
+								System.out.println("Component percent exceeded.");
+							}
+							else if(remain >0&&componentAmount==0){
+								//increase counter to request user add 1 more component
+								componentAmount+=1;
+								System.out.println("Component percent not full.");
+							}
+						}
+						
+						break;
+						
+					default:
+						System.out.println("Invalid Choice");
+						//increase counter to request user add 1 more component
+						componentAmount+=1;
+						break;
+				}//end switch
+				componentAmount--;
+			}
+		}
 }
