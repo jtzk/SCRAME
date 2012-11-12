16bf70b5badd5c2f17f1c8d3d21a7a0b81da227a
tancaibao <tancaibao@gmail.com>
2012-11-13 02:33:53 +0800

    Update src/scrame/ScrameApp.java
    added new case 4. course component

--- src/scrame/ScrameApp.java
+++ src/scrame/ScrameApp.java
@@ -1,39 +1,49 @@
 package scrame;
+
 import java.util.*;
 public class ScrameApp {
 	public static void main(String[] args) {
 		boolean success = false;
 		boolean loaded=false;
-		RawCase rc= new RawCase();
 		char choice = '1';
+		RawCase rc= new RawCase();
+		
 		do {
 			Menu.showMenu();			
 			choice = GetType.getChar();
 			switch (choice) {
-				case '1':
+				case '1':	
 					Professor.menuProfessors();
 					break;
 					
-				case '2':
+				case '2':					
 					Student.menuStudents();
 					break;
 					
-				case '3':
+				case '3':		
 					Course.menuCourse();
 					break;
 					
-				case '4':
+				case'4':
+					System.out.print("Enter course code: ");
+					String _courseCode=GetType.getString();
+					Menu.CourseComponentMenu(_courseCode);
+					
+					break;
+					
+				case '5':
 					if(loaded==false){
 						rc.rawProfessor();
 						rc.rawStudent();
 						rc.rawCourse();
-						rc.rawClass();
+				//		rc.rawClass();
 						rc.rawStudentCourse();
 						loaded=true;
 						System.out.println("Loading rawcases.");
 					}
 					else
 						System.out.println("Rawcases has been loaded.");
+
 					break;
 					
 				case 'q':
@@ -45,4 +55,5 @@ public class ScrameApp {
 			}
 		} while (choice != 'q' && choice != 'Q');
 	}
+
 }
