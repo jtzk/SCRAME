dd0db776411f88d29f5e2c4347cb870000334a50
tancaibao <tancaibao@gmail.com>
2012-11-12 13:24:26 +0800

    Update src/scrame/CourseComponent.java
    Remove mark ! added index .

--- src/scrame/CourseComponent.java
+++ src/scrame/CourseComponent.java
@@ -1,35 +1,67 @@
 package scrame;
 
-public class CourseComponent {
-	private String CourseComponentTitle;
-	private int CourseComponentMark;
-	private int CourseComponentPercent;
+import java.io.Serializable;
+
+public class CourseComponent implements Serializable{
+	private String courseComponentCode;
+	private static int courseComponentPercent;
+	private int index;
 	
-	public CourseComponent(String CourseComponentTitle,int CourseComponentMark , int CourseComponentPercent)
+	public CourseComponent(String _courseComponentCode, int _courseComponentPercent, int _index)
 	{
-		this.CourseComponentTitle=CourseComponentTitle;
-		this.CourseComponentMark=CourseComponentMark;
-		this.CourseComponentPercent=CourseComponentPercent;		
+		courseComponentCode=_courseComponentCode;
+		courseComponentPercent=_courseComponentPercent;	
+		index=_index;
 	}
 
-	public String getCourseComponentTitle()
+	public String getCourseComponentCode()
 	{
-		return CourseComponentTitle;
-	}
-	
-	public int getCourseComponentMark()
-	{
-		return CourseComponentMark;
+		return courseComponentCode;
 	}
 	
 	public int getCourseComponentPercent()
 	{
-		return CourseComponentPercent;
+		return courseComponentPercent;
 	}
 	
-	public int setCalculateScore()
-	{	
-		return CourseComponentPercent*CourseComponentMark;
+	public int getIndex()
+	{
+		return index;
 	}
 	
+//	
+//	public int setCalculateScore()
+//	{	
+//		return courseComponentPercent*courseComponentMark;
+//	}
+//	
+//	public static int processMark(String Component) {
+//		int _mark;
+//		do {
+//			try {
+//				System.out.print("Enter " + Component + "'s mark: ");			
+//				_mark = GetType.getInt();
+//				courseComponentMark=_mark;
+//				return courseComponentMark;
+//			}
+//			catch (NumberFormatException e) {
+//				System.out.println("\n  Error: Invalid mark number. Only digits are allowed.\n");
+//			}
+//		} while (true);
+//	}
+//	
+	public static int processPercent(String Component) {
+		int _percent;
+		do {
+			try {
+				System.out.print("Enter " + Component + "'s percent: ");			
+				_percent = GetType.getInt();
+				courseComponentPercent=_percent;
+				return courseComponentPercent;
+			}
+			catch (NumberFormatException e) {
+				System.out.println("\n  Error: Invalid percent number. Only digits are allowed.\n");
+			}
+		} while (true);
+	}
 }
